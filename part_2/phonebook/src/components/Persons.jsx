1e4ad70f276677c425d3c6daeb6787f7711081bf
Aaro Tuohi <97167063+aarotuohi@users.noreply.github.com>
2024-09-23 19:37:40 +0300

    Part_2 commit

---
+++ part_2/phonebook/src/components/Persons.jsx
@@ -0,0 +1,12 @@
+const Persons = ({ persons, handleDelete }) => (
+  <ul>
+    {persons.map((person) => (
+      <li key={person.name}>
+        {person.name} {person.number}{' '}
+        <button onClick={() => handleDelete(person.id)}>delete</button>
+      </li>
+    ))}
+  </ul>
+);
+
+export default Persons;
